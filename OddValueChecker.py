b20a3e596876750787c4602e66b9a5ab97483dad
Sayamgoyal <sayamgoyal456@gmail.com>
2025-08-20 16:25:57 +0530

    Update OddValueChecker.py

--- OddValueChecker.py
+++ OddValueChecker.py
@@ -1,8 +1,19 @@
-var1 = [1,1,2,2,3,4,4]
+var1 = [1,2,3,4,4,5] 
 def quantity_less_than_one(var1):
-    var2 = []
-    for i in var1:
-        var2.append(var1.count(i))
-        if var1.count(i) < max(var2):
-            print(i)
+    var2 = [] 
+    for j in var1: 
+        var2.append(var1.count(j)) 
+    for i in var1: 
+        if var1.count(i) < max(var2): 
+            print(i) 
+def quantity_more_than_one(var1):
+    var2 = [] 
+    for j in var1: 
+        var2.append(var1.count(j)) 
+    for i in var1: 
+        if var1.count(i) > min(var2): 
+            print(i) 
+            break
+quantity_more_than_one(var1)
+        
     
